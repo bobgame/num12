31824530679609e502bd1c1b5b28d00d01eeb1c1
bobgame <bobgame@users.noreply.github.com>
2025-02-06 02:24:30 +0000

    deploy: 4eb01ffe0bc0fde883a8e18a57afc269255ff516

---
+++ browser/chunk-JVG3O7XJ.js
@@ -0,0 +1,1 @@
+import{Na as o,gb as i,hb as r,tb as m}from"./chunk-C7WIGN6N.js";var p=(()=>{let e=class e{};e.\u0275fac=function(n){return new(n||e)},e.\u0275cmp=o({type:e,selectors:[["nm-num-world"]],decls:2,vars:0,template:function(n,a){n&1&&(i(0,"p"),m(1,"num-world works!"),r())},encapsulation:2});let t=e;return t})();export{p as NumWorldComponent};
