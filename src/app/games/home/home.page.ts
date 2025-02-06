ab50ddd4a367d9422dd95a49f0ce42de034b2616
BobGame <liulingbo@foxmail.com>
2025-02-06 10:51:31 +0800

    remove test navigate

--- src/app/games/home/home.page.ts
+++ src/app/games/home/home.page.ts
@@ -1,9 +1,9 @@
-import { Component } from '@angular/core'
-import { GlobalService } from '../../common/services/global.service'
-import { PageName } from '../../common/enums/base.enum'
-import { CommonModule } from '@angular/common'
-import { TranslateModule } from '@ngx-translate/core'
-import { DialogService } from 'src/app/common/services/dialog.service'
+import { Component } from '@angular/core';
+import { GlobalService } from '../../common/services/global.service';
+import { PageName } from '../../common/enums/base.enum';
+import { CommonModule } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
+import { DialogService } from 'src/app/common/services/dialog.service';
 
 @Component({
   selector: 'nm-home',
@@ -12,19 +12,16 @@ import { DialogService } from 'src/app/common/services/dialog.service'
   imports: [CommonModule, TranslateModule],
 })
 export class HomeComponent {
-  constructor(
-    public g: GlobalService,
-    public dialogService: DialogService,
-  ) {
-    console.log('HomePage constructor')
+  constructor(public g: GlobalService, public dialogService: DialogService) {
+    console.log('HomePage constructor');
 
-    this.g.show.pop.loading = false
+    this.g.show.pop.loading = false;
 
-    this.gotoPage(PageName.numGuess) // for test
+    // this.gotoPage(PageName.numGuess) // for test
   }
-  PageName = PageName
+  PageName = PageName;
 
   gotoPage(pageName: PageName) {
-    this.g.navigate(pageName)
+    this.g.navigate(pageName);
   }
 }
